d2309f78a63af94f5311affb3dccb1f108afd430
Dex Groves <Dex.Groves@gmail.com>
2016-10-30 23:33:51 +0000

    Implement replacement of named arguments

--- R/dang.R
+++ R/dang.R
@@ -1,20 +1,57 @@
-#' Execute the last command with a new function, but the same arguments.
+#' Fix errors in the last command in the history.
 #'
-#' You mistyped it, but don't want to up-arrow, ctrl+a, delete. dang!
+#' Call without one unnamed argument to replace the old calling function with
+#' the new argument. Call with one or more named arguments to replace or
+#' inject new arguments into the last call.
 #'
-#' @param new_fn Raw new function with which to evaluate the old arguments.
+#' @param ... Either a single unnamed argument or a series of named arguments.
 #' @export
 #' @examples
-#' > pritn("Hello world!")
+#' pritn("Hello world!")
 #' # Error: could not find function "pritn"
-#' > dang(print)
-#' # [1] "Hello world!"
-dang <- function(new_fn) {
+#' dang(print)
+#'
+#' sample(letters, size = 26, replace = FALSE)
+#' dang(replace = TRUE)
+dang <- function(...) {
+  this_call <- match.call()
+  args <- as.list(this_call)[2:length(this_call)]
+
+  hist_txt <- retrieve_last_command()
+  hist_call <- parse(text = hist_txt)[[1]]
+
+  if (as.character(hist_call[[1]]) == "dang") {
+    stop("Can't dang on dang!", call. = FALSE)
+  }
+
+  if (is.null(names(args))) {
+    hist_call <- replace_function_name(hist_call, args[[1]])
+  } else {
+    hist_call <- new_named_arguments(hist_call, args)
+  }
+
+  eval.parent(hist_call)
+}
+
+#' Grab the second most recent command in the history
+retrieve_last_command <- function() {
   tmp <- tempfile()
   savehistory(tmp)
   hist_txt <- system(paste("tail -2", tmp), intern = TRUE)[1]
   unlink(tmp)
-  hist_expr <- parse(text = hist_txt)
-  hist_expr[[1]][[1]] <- substitute(new_fn)
-  invisible(eval.parent(hist_expr))
+  hist_txt
+}
+
+#' Replace the top level function name of a call.
+replace_function_name <- function(call, new_fn) {
+  call[[1]] <- new_fn
+  call
+}
+
+#' Replace existing or insert new named arguments into a call.
+new_named_arguments <- function(call, args) {
+  for (i in seq_along(args)) {
+    call[names(args[i])] <- args[i]
+  }
+  call
 }
